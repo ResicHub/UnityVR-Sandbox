2cc527570a99e122cb36c803589ece1bdd350170
Anton Nikolaev <76194166+ResicHub@users.noreply.github.com>
2022-07-04 14:39:44 +0700

    Create test room with working belt.
    Creating new scene, based on "DistanceGrab" scene and make working belt.

---
+++ VR Sandbox/Assets/Scripts/BeltController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BeltController : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    private float speed = 0.5f;
+    private Vector3 starttPosition;
+    private bool isMoving = true;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        starttPosition = myRigidbody.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            myRigidbody.position += Vector3.left * speed * Time.fixedDeltaTime;
+            myRigidbody.MovePosition(starttPosition);
+        }
+    }
+
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+
+    public void SetMovement(bool value)
+    {
+        isMoving = value;
+    }
+}
---
+++ VR Sandbox/Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float respawnTime = 1;
+    private float timer;
+
+    [SerializeField]
+    private List<GameObject> prefabs;
+
+    public bool isSpawning = true;
+    void Start()
+    {
+        timer = respawnTime;
+    }
+
+    void FixedUpdate()
+    {
+        if (isSpawning)
+        {
+            timer -= Time.fixedDeltaTime;
+            if (timer < 0)
+            {
+                Spawn();
+                timer = respawnTime;
+            }
+        }
+    }
+
+    public void SetRespawn(float value)
+    {
+        respawnTime = value;
+    }
+
+    private void Spawn()
+    {
+        Instantiate(prefabs[Random.Range(0, prefabs.Count)],
+                    transform.position + new Vector3(0, 0, Random.value - 0.5f),
+                    Quaternion.Euler(0, Random.Range(-180, 180), 0),
+                    transform);
+    }
+}
